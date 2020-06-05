d7f74db9b6e154d6c0112681a50a509593025e61
Mego4el <meg4el.ivan@gmail.com>
2020-06-05 23:33:15 +0700

    Initial commit

---
+++ lab1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PassUsingModel()
+        {
+            var viewModel = new RandGen();
+            return View(viewModel);
+        }
+
+        public IActionResult PassUsingViewData()
+        {
+            ViewData["RandGen"] = new RandGen();
+            return View();
+        }
+
+        public IActionResult PassUsingViewBag()
+        {
+            ViewBag.RandGen = new RandGen();
+            return View();
+        }
+
+        public IActionResult AccessServiceDirectly()
+        {
+            return View();
+        }
+
+
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ lab1/WebApplication1/Models/RandGen.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class RandGen
+    {
+        static Random rnd = new Random();
+        public int val1;
+        public int val2;
+        public int add;
+        public int sub;
+        public int div;
+        public int mul;
+        public RandGen()
+        {
+            val1 = rnd.Next(0, 10);
+            val2 = rnd.Next(0, 10);
+            add = val1 + val2;
+            sub = val1 - val2;
+            if (val2 != 0)
+            {
+                div = val1 / val2;
+            }
+            else { div = -1; }
+            mul = val1 * val2;
+        }
+    }
+}
