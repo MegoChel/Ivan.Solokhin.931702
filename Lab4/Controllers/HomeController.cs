38340cf68859be327e70e033e64b8ce8b8899f62
Mego4el <meg4el.ivan@gmail.com>
2020-06-07 15:16:36 +0700

    init commit

---
+++ Lab4/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Lab4.Models;
+
+namespace Lab4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Controls()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Lab4/Models/ControlModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab4.Models
+{
+    public class ControlModel
+    {
+        public string Result { get; set; }
+        public string ControlElement { get; set; }
+        public string Name { get; set; }
+    }
+}
